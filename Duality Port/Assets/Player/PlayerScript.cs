944027c6c72eba40d12bffb14cae2e8d49270cd0
Jeremy Maldonado <jermaldo@iu.edu>
2023-04-13 17:31:00 -0400

    yang anims

--- Duality Port/Assets/Player/PlayerScript.cs
+++ Duality Port/Assets/Player/PlayerScript.cs
@@ -13,15 +13,23 @@ public class PlayerScript : MonoBehaviour
     private bool isGrounded;
     private bool isLunging;
     private bool facingRight;
-
     public bool isYang;
+
+    public bool animEnd;
 
     [SerializeField] float timer;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
+    [SerializeField] private Transform attackPoint;
     [SerializeField] private LayerMask groundLayers;
+    [SerializeField] private LayerMask enemyLayers;
+    
     [SerializeField] Animator animator;
-    [SerializeField] Sprite kickSprite;
+    [SerializeField] GameObject shadow;
+
+    private float apex = 4.05f, st = 2.275f;//Hard coded values, if position of object change, this needs to as well
+    
+    private float shadowOpac;
     
 
     // Start is called before the first frame update
@@ -35,21 +43,25 @@ public class PlayerScript : MonoBehaviour
         isCharging = false;
         isLunging = false;
         isYang = false;
+        animEnd = true;
     }
 
-    // Update is called once per frame
+    // Update is called once per fram6f
     void Update()
     {   
+        shadowOpac = 1 - ((transform.position.y - st) / (apex - st));
+        shadow.transform.position = new Vector3(transform.position.x, st - .6f, transform.position.z + 1);    
+        shadow.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, shadowOpac);
+
         isGrounded = IsGrounded();
         animator.SetBool("Grounded", isGrounded);
         if (IsGrounded() && !hasDoubleJump) {
             hasDoubleJump = true;
         }
-        if (!isCharging) {
+        if (!isCharging && !isLunging) {
             horizontal = Input.GetAxisRaw("Horizontal");
             rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
         }
-        
 
         animator.SetFloat("Velocity", Mathf.Abs(horizontal));
         animator.SetFloat("JumpVel", rb.velocity.y);
@@ -74,8 +86,11 @@ public class PlayerScript : MonoBehaviour
         if(Input.GetButton("YangAttack")) {
             animator.SetBool("IsYang", true);
             isYang = true;
+            animEnd = false;
 
-            
+            //check here what anim is playing,
+            //if left is playing, switch to right and vice versa
+            //do collides here as well
         }
         if (Input.GetButton("YinAttack")) {
             animator.SetBool("IsYang", false);
@@ -98,13 +113,21 @@ public class PlayerScript : MonoBehaviour
             isLunging = true;
             animator.SetBool("IsLunging", true);
         }
-        //is lunging
         
         if(isLunging) {
             timer += Time.deltaTime;
             int dir = facingRight ? 1 : -1;
             rb.velocity = new Vector2(slideSpeed * charge * dir, rb.velocity.y);
-            if (timer > 1) {
+
+
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, .5f, enemyLayers);
+            foreach(Collider2D enemy in hitEnemies) {
+                Debug.Log("hit ene" + enemy.name);
+
+                //HERE CHECK WHAT COLOR ENEMIES ARE AND DO CALCS FOR DAMAGE
+            }
+
+            if (timer > charge*1.2) {
                 rb.velocity = Vector2.zero;
                 isLunging = false;
                 timer = 0;
@@ -115,23 +138,26 @@ public class PlayerScript : MonoBehaviour
 
 
         //pther attack
-        //GetCurrentAnimatorClipInfo
-        //If double punch anim is playhing && button is pressed
-        //do double punch again
+        //anim1, left punch
+        //anim2, right punch
+        //once anim1 is done, go to 2
+        //animator event at end of 2,
+        //if button pressed at any time, go back to anim1
+        //if event, do uppercut
+
+          
     }   
 
     bool IsGrounded() {
         return Physics2D.OverlapCircle(groundCheck.position, .1f, groundLayers);
     }
 
-
-    private void YinAttack() {
-        //find some way to smoothly move across the floor
-        Debug.Log("hori"+ horizontal);
-        rb.velocity = new Vector2(10f * moveSpeed, 0f);
-    }
-
     private void TakeData() {
         
+    }
+
+    private void YangAnimEnd() {
+        animEnd = true;
+        //start upercut here
     }
 }
