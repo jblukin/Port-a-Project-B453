6747f4bf2dce55d842120e9329769d71b664f02e
jermaldo <jermaldo@iu.edu>
2023-04-13 10:08:44 -0400

    slide kick

--- Duality Port/Assets/Player/PlayerScript.cs
+++ Duality Port/Assets/Player/PlayerScript.cs
@@ -5,22 +5,36 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {   
     private int moveSpeed;
+    private float charge;
     private float horizontal;
+    private float slideSpeed;
+    private bool isCharging;
     private bool hasDoubleJump;
     private bool isGrounded;
+    private bool isLunging;
+    private bool facingRight;
 
+    public bool isYang;
+
+    [SerializeField] float timer;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayers;
     [SerializeField] Animator animator;
+    [SerializeField] Sprite kickSprite;
     
 
     // Start is called before the first frame update
     void Start()
     {
         moveSpeed = 7;
+        timer = 0;
+        slideSpeed = 6.5f;
         hasDoubleJump = true;
         isGrounded = true;
+        isCharging = false;
+        isLunging = false;
+        isYang = false;
     }
 
     // Update is called once per frame
@@ -31,16 +45,22 @@ public class PlayerScript : MonoBehaviour
         if (IsGrounded() && !hasDoubleJump) {
             hasDoubleJump = true;
         }
-        horizontal = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+        if (!isCharging) {
+            horizontal = Input.GetAxisRaw("Horizontal");
+            rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+        }
+        
 
         animator.SetFloat("Velocity", Mathf.Abs(horizontal));
         animator.SetFloat("JumpVel", rb.velocity.y);
+        animator.SetBool("IsCharging", isCharging);
 
         if (horizontal < 0) {
             transform.localScale = new Vector3(-2, 2, 2);
+            facingRight = false;
         }else if (horizontal > 0) {
             transform.localScale = new Vector3(2, 2, 2);
+            facingRight = true;
         }
 
         if (Input.GetButtonUp("Jump") && isGrounded) {
@@ -51,20 +71,67 @@ public class PlayerScript : MonoBehaviour
         }
 
 
-        if(Input.GetButtonUp("YangAttack")) {
+        if(Input.GetButton("YangAttack")) {
             animator.SetBool("IsYang", true);
-            YangAttack();
+            isYang = true;
+
+            
         }
-        if (Input.GetButtonUp("YinAttack")) {
+        if (Input.GetButton("YinAttack")) {
             animator.SetBool("IsYang", false);
+            rb.velocity = new Vector2(0, 0);
+            animator.SetFloat("Velocity", 0);
+            animator.SetFloat("JumpVel", 0);
+
+            if (timer < 1) {
+                timer += Time.deltaTime;
+                isCharging = true;
+                animator.SetBool("IsCharging", true);
+            }
         }
+
+        if (Input.GetButtonUp("YinAttack") && isCharging) {
+            charge = timer;
+            timer = 0;
+            isCharging = false;
+            animator.SetBool("IsCharging", false);
+            isLunging = true;
+            animator.SetBool("IsLunging", true);
+        }
+        //is lunging
+        
+        if(isLunging) {
+            timer += Time.deltaTime;
+            int dir = facingRight ? 1 : -1;
+            rb.velocity = new Vector2(slideSpeed * charge * dir, rb.velocity.y);
+            if (timer > 1) {
+                rb.velocity = Vector2.zero;
+                isLunging = false;
+                timer = 0;
+                charge = 0;
+                animator.SetBool("IsLunging", false);
+            }
+        }
+
+
+        //pther attack
+        //GetCurrentAnimatorClipInfo
+        //If double punch anim is playhing && button is pressed
+        //do double punch again
     }   
 
     bool IsGrounded() {
         return Physics2D.OverlapCircle(groundCheck.position, .1f, groundLayers);
     }
 
-    private void YangAttack() {
+
+    private void YinAttack() {
+        //find some way to smoothly move across the floor
+        Debug.Log("hori"+ horizontal);
+        rb.velocity = new Vector2(10f * moveSpeed, 0f);
+    }
+
+    private void TakeData() {
         
     }
 }
